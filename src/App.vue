d175e1fb2bc4df9d8bab31b81d1be91896f2ff15
Taher <taher.mulla@gmail.com>
2025-11-09 14:13:03 -0600

    auth added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-import Todos from './components/Todos.vue'
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+import Todos from "./components/Todos.vue";
 </script>
 
 <template>
-  <main>
-    <Todos />
-  </main>
+  <Authenticator>
+    <template #default="{ signOut }">
+      <main class="app-shell">
+        <header class="app-header">
+          <button type="button" @click="signOut">Sign out</button>
+        </header>
+        <Todos />
+      </main>
+    </template>
+  </Authenticator>
 </template>
 
